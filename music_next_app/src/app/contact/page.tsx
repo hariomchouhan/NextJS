d8654d2ac1dcfa09a19ff6b9a05d458884b4e876
Hariom Chouhan <hariomchouhan430@gmail.com>
2024-08-30 21:02:09 +0930

    add Meteor in contact page but it is not fix properly

--- music_next_app/src/app/contact/page.tsx
+++ music_next_app/src/app/contact/page.tsx
@@ -1,8 +1,12 @@
+import { Meteors } from '@/components/ui/meteors'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page2</div>
+    <main className="bg-blue-700 flex justify-center items-center mx-auto h-full w-full">
+       {/* Meaty part - Meteor effect */}
+       <Meteors number={20} />
+    </main>
   )
 }
 
