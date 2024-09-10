ed326b4fe976ddad29c24156afe5ef7ef1970a2e
Hariom Chouhan <hariomchouhan430@gmail.com>
2024-09-10 18:30:32 +0930

    Change domain variable name in .env

--- next_auth/src/helpers/mailer.ts
+++ next_auth/src/helpers/mailer.ts
@@ -34,8 +34,8 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
             from: 'hariomchouhan430@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            html: `<p>Click <a href="${process.env.NEXT_PUBLIC_API_URL}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.NEXT_PUBLIC_API_URL}/verifyemail?token=${hashedToken}
             </p>`,
         }
 
